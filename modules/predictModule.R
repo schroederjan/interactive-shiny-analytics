bac8206dcc4bff2232c13a71d27943af7147fa6b
AionosChina <d8a@posteo.us>
2020-10-23 12:02:47 +0200

    fixed predictModule, full function now

--- modules/predictModule.R
+++ modules/predictModule.R
@@ -1,5 +1,7 @@
 # Predict Module
 
+source("packages.R")
+
 #
 #USER INTERFACE
 #
@@ -33,104 +35,89 @@ predictServer <- function(id, data) {
       #PI <- F
       #NPATHS <- 100
       
-      prediction_function <- function(data, h, pi, npaths) {
-        
-        ts <- data %>% read.zoo()
-        
-        #NNETAR FUNCTION
-        model.nnetar.fc <- function(data, h, pi, npaths){
-          pred_tbl <- data %>% 
-            nnetar() %>% 
-            {. ->> result.model } %>%  
-            forecast(h = h, PI = pi, npaths = npaths) %>% 
-            {. ->> result.fc } %>%  
-            data.frame() %>% 
-            {. ->> result.df } 
-        }
-        
-        #nice function to get everything out of the workflow
-        model.nnetar.fc(data$value, h = H, pi = PI, npaths = NPATHS)
-        #result as df uncomment:
-        #result.df
-        #result as plot uncomment:
-        #autoplot(result.fc)
-        #result as model uncomment:
-        #result.model
-        
-        # create future dates for the time horizon predicted
-        idx <- ts %>%
-          tk_index() %>%
-          tk_make_future_timeseries(length_out = H)
-        #idx
-        
-        # Retransform values
-        result.tbl <- tibble(
-          index   = idx,
-          value   = result.df$Point.Forecast)
-        #result.tbl
-        #str(result.tbl)
-        
-        #easy function to avoid double code
-        ts_to_df <- function(start_ts) {
-          end_df <- start_ts %>% 
-            tk_tbl() %>% 
-            mutate(index = as_date(index)) %>%
-            as_tbl_time(index = index) %>% 
-            data.frame()
-        }
-        #add "actual" string for actuals and "predict for prediction df to be transformed to ts for visuals
-        df_to_ts <- function(start_df, actual_predict) {
-          end_ts <- start_df %>% 
-            filter(key == actual_predict) %>% 
-            select(index, value) %>% 
-            read.zoo() 
-        }
-        
-        df.act <- ts_to_df(ts) %>% add_column(key = "actual")
-        df.fc <- ts_to_df(result.tbl) %>% add_column(key = "predict")
-        #RESULT AS DF
-        df.result <- rbind(df.act, df.fc)
-        
-        ts.act <- df_to_ts(df.result, "actual")
-        #str(ts.act)
-        ts.fc <- df_to_ts(df.result, "predict")
-        #str(ts.fc)
-        #RESULT AS TS
-        ts.result <- merge(ts.act, ts.fc)
-        names(ts.result) <- c("Actual", "Prediction")
-        
-        #add fit from the model to the results
-        ts.fit <- data.frame(df.act, result.fc$fitted) %>% 
-          select("index","result.fc.fitted") %>%
-          na.omit() %>% 
-          read.zoo()
-        names(ts.fit) <- c("index", "Fit")
-        
-        #RESULT EXTENDED AS TS
-        ts.extended <- merge(ts.result, ts.fit)
-        names(ts.extended) <- c("Actual", "Prediction", "Fit")
-        
-        # Accuracy
-        ts.accuracy <- accuracy(result.fc)
-        
-        # Simple Plot
-        ts.plot <- plot(result.fc)
-        
-        # Residuals of the Model
-        #res.plot <- result.model$residuals %>% plot(main = "Plot of Residuals")
-        #res.acf <- result.model$residuals %>% na.omit() %>% Acf(main = "ACF of Residuals")
-        #res.hist <- result.model$residuals %>% hist(main = "Histogram of Residuals")
-        
-        module.list <- list(ts.extended, ts.accuracy, ts.plot, result.model)
-        
-        return(module.list)
-        
+      ts <- data %>% read.zoo()
+      
+      #NNETAR FUNCTION
+      model.nnetar.fc <- function(data, h, pi, npaths){
+        pred_tbl <- data %>% 
+          nnetar() %>% 
+          {. ->> result.model } %>%  
+          forecast(h = h, PI = pi, npaths = npaths) %>% 
+          {. ->> result.fc } %>%  
+          data.frame() %>% 
+          {. ->> result.df } 
       }
       
-      ts.list <- prediction_function(data, h = H, pi = PI, npaths = NPATHS)
+      #nice function to get everything out of the workflow
+      model.nnetar.fc(data$value, h = H, pi = PI, npaths = NPATHS)
+      
+      # create future dates for the time horizon predicted
+      idx <- ts %>%
+        tk_index() %>%
+        tk_make_future_timeseries(length_out = H)
+      #idx
+      
+      # Retransform values
+      result.tbl <- tibble(
+        index   = idx,
+        value   = result.df$Point.Forecast)
+      #result.tbl
+      #str(result.tbl)
+      
+      #easy function to avoid double code
+      ts_to_df <- function(start_ts) {
+        end_df <- start_ts %>% 
+          tk_tbl() %>% 
+          mutate(index = as_date(index)) %>%
+          as_tbl_time(index = index) %>% 
+          data.frame()
+      }
+      #add "actual" string for actuals and "predict for prediction df to be transformed to ts for visuals
+      df_to_ts <- function(start_df, actual_predict) {
+        end_ts <- start_df %>% 
+          filter(key == actual_predict) %>% 
+          select(index, value) %>% 
+          read.zoo() 
+      }
+      
+      df.act <- ts_to_df(ts) %>% add_column(key = "actual")
+      df.fc <- ts_to_df(result.tbl) %>% add_column(key = "predict")
+      #RESULT AS DF
+      df.result <- rbind(df.act, df.fc)
+      
+      ts.act <- df_to_ts(df.result, "actual")
+      #str(ts.act)
+      ts.fc <- df_to_ts(df.result, "predict")
+      #str(ts.fc)
+      #RESULT AS TS
+      ts.result <- merge(ts.act, ts.fc)
+      names(ts.result) <- c("Actual", "Prediction")
+      
+      #add fit from the model to the results
+      ts.fit <- data.frame(df.act, result.fc$fitted) %>% 
+        select("index","result.fc.fitted") %>%
+        na.omit() %>% 
+        read.zoo()
+      names(ts.fit) <- c("index", "Fit")
+      
+      #RESULT EXTENDED AS TS
+      ts.extended <- merge(ts.result, ts.fit)
+      names(ts.extended) <- c("Actual", "Prediction", "Fit")
+      
+      # Accuracy
+      ts.accuracy <- accuracy(result.fc) %>% data.frame()
+      #ts.accuracy <- result.fc
+      #accuracy(result.fc)
+      
+      # Simple Plot
+      #ts.plot <- plot(result.fc)
+      ts.plot <- result.fc
+      
+      module.list <- list(ts.extended, ts.accuracy, ts.plot, result.model)
+      
+      return(module.list)
       
     })
-    
     # Return the reactive that yields the timeseries
     return(ts.list)
     
@@ -142,14 +129,9 @@ predictServer <- function(id, data) {
 ui <- fluidPage(
   
   fluidRow(
-    column(8,
-           h4("Interactive Plot"),
+    column(12,
+           h3("Interactive Plot for the Overview"),
            dygraphOutput("dygraph.fc")
-    ),
-    
-    column(4,
-           h4("Prediction Interval Plot (PI)"),
-           plotOutput("plot.fc")
     )
   ),
   
@@ -157,21 +139,29 @@ ui <- fluidPage(
   
   fluidRow(
     column(6,
-           h4("Model Configurations"),
+           h3("Model Configurations for the Prediction Model"),
            predictUI("predictModule")
-    ),
+           ),
     
     column(6,
-           h4("Training Accuracy"),
-           tableOutput("acc.tr")
-    )
-  ),
-  
+           h3("Training Accuracy (From Model Fit)"),
+           tableOutput("acc.tr"),
+           
+           h3("Testing Accuracy (From Crossvalidation)"),
+           #tableOutput("acc.tr")
+           )
+    ),
+    
   fluidRow(
-    column(12,
-           h4("Model Residuals"),
+    column(8,
+           h3("Model Residuals (What patterns are left after using the Model)"),
            plotOutput("plot.res")
+    ),
+    column(4,
+           h3("Prediction Interval Plot (PI)"),
+           plotOutput("plot.fc")
     )
+    
   )
 )
             
@@ -180,30 +170,55 @@ server <- function(input, output, session) {
   data <- read.csv("/data/R/Projects/interactive-shiny-analytics/data/raw/MONTH CN House Prices Monthly.csv",
                    stringsAsFactors=FALSE)
   
-  output$dygraph.fc <- renderDygraph({
+  timeseries  <- reactive({
     ts.list <- predictServer("predictModule", data)
-    ts <- ts.list()[1] %>% data.frame()
-    dygraph(ts)
   })
   
-  output$accuracy.tr <- renderTable({
-    ts.list <- predictServer("predictModule", data)
-    ts.acc <- ts.list()[2] %>% data.frame()
-    
-  })
-  
-  output$plot.fc <- renderPlot({
-    ts.list <- predictServer("predictModule", data)
-    ts.plot <- ts.list()[3][[1]]
-  })
-  
-  output$plot.res <- renderPlot({
-    ts.list <- predictServer("predictModule", data)
-    ts.model <- ts.list()[4][[1]]
-    ts.model %>% checkresiduals()
-  
-  })
-  
+   output$dygraph.fc <- renderDygraph({
+     ts.list <- timeseries()
+     ts <- ts.list()[1] %>% data.frame()
+     dygraph(ts)
+   })
+   
+   output$acc.tr <- renderTable({
+     ts.list <- timeseries()
+     ts.acc <- ts.list()[[2]]
+   })
+   
+   output$plot.fc <- renderPlot({
+     ts.list <- timeseries()
+     ts.plot <- plot(ts.list()[[3]])
+   })
+
+   output$plot.res <- renderPlot({
+     ts.list <- timeseries()
+     ts.model <- ts.list()[4][[1]]
+     ts.model %>% checkresiduals()
+   })
+   
+  # output$dygraph.fc <- renderDygraph({
+  #   ts.list <- predictServer("predictModule", data)
+  #   ts <- ts.list()[1] %>% data.frame()
+  #   dygraph(ts)
+  # })
+
+  # output$accuracy.tr <- renderTable({
+  #   ts.list <- predictServer("predictModule", data)
+  #   ts.acc <- ts.list()[2] %>% data.frame()
+  # 
+  # })
+  # 
+  # output$plot.fc <- renderPlot({
+  #   ts.list <- predictServer("predictModule", data)
+  #   ts.plot <- ts.list()[3][[1]]
+  # })
+  # 
+  # output$plot.res <- renderPlot({
+  #   ts.list <- predictServer("predictModule", data)
+  #   ts.model <- ts.list()[4][[1]]
+  #   ts.model %>% checkresiduals()
+  # })
+
 }
 
 shinyApp(ui, server)
